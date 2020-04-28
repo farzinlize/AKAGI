d2bee2a18c900246ee7d1e2fdf83c7b894d9bbc1
Farzin Mohammadi <farzinlize@live.com>
2020-04-28 20:01:51 +0430

    generate neighbours kmers in Nodmer

--- Nodmer.py
+++ Nodmer.py
@@ -4,7 +4,21 @@ class Nodmer:
     def __init__(self, gkhood, kmer):
         self.graph = gkhood
         self.kmer = kmer
+        self.neighbours = []
 
     
     def generate_neighbours(self):
-        pass
+        result = []
+        for i in range(len(self.kmer)):
+            #delete
+            result += [self.kmer[:i] + self.kmer[i+1:]]
+            
+            for letter in self.graph.alphabet:
+                #insert
+                result += [self.kmer[:i] + letter + self.kmer[i:]]
+                #sub
+                if letter != self.kmer[i]:
+                    result += [self.kmer[:i] + letter + self.kmer[i+1:]]
+        
+        #last insertions
+        return result + [self.kmer + letter for letter in self.graph.alphabet]
