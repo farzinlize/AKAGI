1695b785d2554e60c43a0a34ed4db93c030ee38d
Farzin Mohammadi <farzinlize@live.com>
2020-04-28 20:02:34 +0430

    add find function for TrieFind

--- TrieFind.py
+++ TrieFind.py
@@ -5,21 +5,42 @@ class TrieNode:
         self.label = lable
         self.childs = []
         self.level = level
+        self.hasNode = False
 
 
     def is_root(self):
         return self.level == 0
 
 
+    def get_node(self):
+        if not self.hasNode:
+            raise Exception("this trie has no node")
+        return self.node
+
+
+    def create_node(self, gkhood):
+        self.node = Nodmer(gkhood, self.label)
+        self.hasNode = True
+        return self.node
+
+
     def child_birth(self, gkhood):
         if self.level == gkhood.kmax:
-            return [Nodmer(gkhood, self.label)]
+            return [self.create_node(gkhood)]
         self.childs = [TrieNode(self.label + letter, self.level+1) for letter in gkhood.alphabet]
         grandchildsNodmers = []
         for child in self.childs:
             grandchildsNodmers += child.child_birth(gkhood)
         if self.level >= gkhood.kmin:
-            grandchildsNodmers += [Nodmer(gkhood, self.label)]
+            grandchildsNodmers += [self.create_node(gkhood)]
         return grandchildsNodmers
 
 
+    def find(self, kmer):
+        if len(kmer) == 0:
+            return self.get_node()
+        if len(self.childs) == 0:
+            return None
+        for child in self.childs:
+            if child.label[-1] == kmer[0]:
+                return child.find(kmer[1:])
