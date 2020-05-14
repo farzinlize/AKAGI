af0e7721289f7834982bc305155db43e1dc76c77
Farzin Mohammadi <farzinlize@live.com>
2020-05-14 10:31:24 +0430

    add a function to determine edit distance of kmers in scripts (new file)

---
+++ scripts.py
@@ -0,0 +1,20 @@
+def edistance(kmer, lmer):
+    if len(kmer) == 0:
+        return len(lmer)
+    if len(lmer) == 0:
+        return len(kmer)
+    if kmer[0] == lmer[0]:
+        return edistance(kmer[1:], lmer[1:])
+    return min(edistance(kmer[1:], lmer)+1, edistance(kmer, lmer[1:])+1, edistance(kmer[1:], lmer[1:])+1)
+
+
+def main():
+    kmer = 'GTCGCAGTCGCTGCC'
+    lmer = 'CGCTGCCACCGCTG'
+    pmer = 'CAGCGGCTGCGGA'
+
+    print(edistance(pmer, lmer))
+    print(edistance(pmer, kmer))
+
+
+main()
