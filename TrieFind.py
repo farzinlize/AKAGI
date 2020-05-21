7891aa25398cdb0e1f32017c8d15a100c3c2840b
Farzin Mohammadi <farzinlize@live.com>
2020-05-21 12:28:57 +0430

    add functions to TrieNode for motif-finding tree (also binery add)

--- TrieFind.py
+++ TrieFind.py
@@ -44,3 +44,57 @@ class TrieNode:
         for child in self.childs:
             if child.label[-1] == kmer[0]:
                 return child.find(kmer[1:])
+
+
+    def add_frame(self, kmer, seq_id):
+        if len(kmer) == 0:
+            if not hasattr(self, 'found_list'):
+                self.found_list = []
+            self.found_list = binery_add(self.found_list, seq_id)
+            return
+        for child in self.childs:
+            if child.label[-1] == kmer[0]:
+                return child.add_frame(kmer[1:], seq_id)
+        # proper child dose not exist
+        new_child = TrieNode(self.label + kmer[0], self.level+1)
+        self.childs += [new_child]
+        return new_child.add_frame(kmer[1:], seq_id)
+
+    
+    def extract_motif(self, q):
+        motifs = []
+        if hasattr(self, 'found_list'):
+            if len(self.found_list) >= q:
+                motifs += [self.label]
+        for child in self.childs:
+            motifs += child.extract_motif(q)
+        return motifs
+        
+        
+
+
+def binery_add(lst, item):
+    start = 0
+    end = len(lst)-1
+    while start <= end:
+        mid = (start+end)//2
+        if lst[mid] == item:
+            return lst
+        elif lst[mid] < item:
+            start = mid + 1
+        else:
+            end = mid - 1
+    return lst[:start] + [item] + lst[start:]
+
+
+def test_main():
+    lst = []
+    lst = binery_add(lst, 5)
+    lst = binery_add(lst, 8)
+    lst = binery_add(lst, 2)
+    lst = binery_add(lst, 3)
+    lst = binery_add(lst, 10)
+    lst = binery_add(lst, 3)
+    print(lst)
+
+# test_main()
