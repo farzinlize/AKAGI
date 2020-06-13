3c1a5f2f0d3e378e947b571a19205ae35b6154f7
Farzin Mohammadi <farzinlize@live.com>
2020-06-13 12:28:38 +0430

    move GKhoodTree interface object to GKmerhood module

--- GKmerhood.py
+++ GKmerhood.py
@@ -1,7 +1,7 @@
 from TrieFind import TrieNode
 from Nodmer import Nodmer
-from misc import FileHandler, heap_decode
-import json
+from misc import FileHandler, heap_decode, heap_encode, alphabet_to_dictionary
+import os, platform, json
 
 '''
     Graph object -> hold all information of nodes and graph properties
@@ -99,6 +99,85 @@ class GKmerhood:
             meta.write(json.dumps(metadata_dict))
 
 
+'''
+    Graph interface object -> responsible for returning d-neighbourhood, extracted from GKhood
+        if an instance of gkhood is already in memory, the object will use the instance
+        and if not, the object will extract a location of requested data from a tree-like file
+'''
+class GKHoodTree:
+    def __init__(self, filename='', directory_name='dataset', gkhood=None):
+
+        # gkhood in memory
+        if gkhood != None:
+            self.gkhood = gkhood
+
+        else:
+            # file address spliter is different in windows and linux
+            operating_system = platform.system()
+            if operating_system == 'Windows':
+                spliter = '\\'
+            else:   # operating_system == 'Linux'
+                spliter = '/'
+
+            self.read_metadata(filename)
+            self.read_tree(filename)
+            self.directory = os.getcwd() + spliter + directory_name + spliter
+            self.dictionary = alphabet_to_dictionary(self.metadata['alphabet'])
+    
+
+    def read_tree(self, filename):
+        self.tree = []
+        with open(filename + '.tree', 'r') as tree_file:
+            for line in tree_file:
+                row = line.split()
+                self.tree += [(row[0], int(row[1]))]
+
+
+    def read_metadata(self, filename):
+        with open(filename + '.metadata', 'r') as metafile:
+            self.metadata = json.load(metafile)
+
+
+    def dneighbours_mem(self, kmer, dmax):
+        node = self.gkhood.trie.find(kmer)
+        return node.dneighbours(dmax)
+
+
+    def dneighbours(self, kmer, dmax):
+
+        # from memory
+        if hasattr(self, 'gkhood'):
+            return self.dneighbours_mem(kmer, dmax)
+
+        # from dataset
+        line = heap_encode(kmer, self.dictionary) - self.metadata['bias']
+        dn_length = self.tree[line+1][1] - self.tree[line][1]
+        dneghbours = []
+        with open(self.directory + self.tree[line][0] + '.data', 'r') as host:
+
+            # skip lines
+            for _ in range(self.tree[line][1]):
+                next(host)
+            
+            # read part
+            for _ in range(dn_length):
+                line_data = host.readline().split()
+                if int(line_data[1]) > dmax:
+                    break
+                dneghbours += [(line_data[0], int(line_data[1]))]
+        
+        return dneghbours
+
+
+    def get_position(self, kmer):
+
+        # return None if there is no dataset
+        if hasattr(self, 'gkhood'):
+            return None
+
+        line = heap_encode(kmer, self.dictionary) - self.metadata['bias']
+        return self.tree[line]
+        
 
 # ########################################## #
 #           main fucntion section            #
