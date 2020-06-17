1f3a7873ee7985670faaafaeeb8586b4ff640c3e
Farzin Mohammadi <farzinlize@live.com>
2020-06-17 20:50:34 +0430

    fix and re-design report functions

--- report.py
+++ report.py
@@ -3,51 +3,79 @@ from matplotlib import pyplot
 from misc import Queue
 import numpy
 
-def location_histogram_main(motifs, sequences, frame_size):
-
-    print('number of motifs->', len(motifs))
-    
-    bins = numpy.linspace(0, max([len(s) for s in sequences]), max([len(s) for s in sequences]))
-
-    histogram_lists = [reduce((lambda x,y:x+y), [motif.found_list[1][i] for motif in motifs]) for i in range(len(sequences))]
-
-    alpha = 1 / len(sequences)
-    for i in range(len(sequences)):
-        pyplot.hist(histogram_lists[i], bins, alpha=alpha, label='seq_'+str(i))
-
-    # pyplot.hist(histogram_lists[0], bins, alpha=0.5, label='seq_1')
-
-    pyplot.legend(loc='upper right')
-    pyplot.show()
-
-
-def location_histogram(motifs, sequences, sequence_mask):
-    print('starting to show location histogram (length of lst is %d )'%len(motifs))
+def location_histogram(motifs, sequences, sequence_mask, save=True, savefilename='figure.png'):
     bins = numpy.linspace(0, max([len(s) for s in sequences]), max([len(s) for s in sequences]))
     histogram_lists = [reduce((lambda x,y:x+y), [motif.found_list[1][i] for motif in motifs]) for i in range(len(sequences))]
-    alpha = 1 / (reduce((lambda x,y:x+y), sequence_mask))
+    alpha = 1 / (reduce((lambda x,y:int(x)+int(y)), sequence_mask))
     for i in range(len(sequences)):
         if sequence_mask[i]:
             pyplot.hist(histogram_lists[i], bins, alpha=alpha, label='seq_'+str(i))
     pyplot.legend(loc='upper right')
-    pyplot.show()
-    print('exiting the plot function')
+    
+    if save:
+        pyplot.savefig(savefilename)
+    else:
+        pyplot.show()
 
 
 def motif_chain_report(motifs, filename):
-    with open(filename, 'w') as report:
-        queue = Queue(motifs)
-        current_level = 0
-        report.write('> 1-chained\n')
-        while not queue.isEmpty():
-            link = queue.pop()
-            if current_level < link.chain_level:
-                current_level = link.chain_level
-                report.write('> %d-chained\n'%(current_level+1))
-            report.write(link.chain_sequence()+'\n')
-            for child in link.next_chains:
-                queue.insert(child)
+    report_kmer = open(filename + '.labels', 'w')
+    report_locations = open(filename + '.locations', 'w')
 
+    queue = Queue(motifs)
+    current_level = 0
+    report_kmer.write('> 1-chained\n')
+    report_locations.write('> 1-chained\n')
+    while not queue.isEmpty():
+        link = queue.pop()
+        if current_level < link.chain_level:
+            current_level = link.chain_level
+            report_kmer.write('> %d-chained\n'%(current_level+1))
+            report_locations.write('> %d-chained\n'%(current_level+1))
+        report_kmer.write(link.chain_sequence()+'\n')
+        report_locations.write(link.chain_locations_str()+'\n')
+        for child in link.next_chains:
+            queue.insert(child)
+
+    report_kmer.close()
+    report_locations.close()
+
+# ########################################## #
+#           main fucntion section            #
+# ########################################## #
+
+def test_reduce():
+    # mask = '110011111000021'
+    mask = [1, 0, 0, 1, 0, 1, 1, 0]
+    print(reduce(lambda x,y:int(x)+int(y), mask))
+
+def test_savefig():
+    # line 1 points 
+    x1 = [1,2,3] 
+    y1 = [2,4,1] 
+    # plotting the line 1 points  
+    pyplot.plot(x1, y1, label = "line 1") 
+  
+    # line 2 points 
+    x2 = [1,2,3] 
+    y2 = [4,1,3] 
+    # plotting the line 2 points  
+    pyplot.plot(x2, y2, label = "line 2") 
+    
+    # naming the x axis 
+    pyplot.xlabel('x - axis') 
+    # naming the y axis 
+    pyplot.ylabel('y - axis') 
+    # giving a title to my graph 
+    pyplot.title('Two lines on same graph!') 
+    
+    # show a legend on the plot 
+    pyplot.legend() 
+    
+    # function to show the plot 
+    import os
+    os.mkdir('.\\results\\new')
+    pyplot.savefig('.\\results\\new\\s.png') 
 
 if __name__ == "__main__":
-    location_histogram(1, 6)
+    test_reduce()
