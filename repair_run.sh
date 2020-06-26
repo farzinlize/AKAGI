f3b0d77c9214da0c108d50e1a9974a76abb5581a
Farzin Mohammadi <farzinlize@live.com>
2020-06-26 16:09:57 +0430

    removing .sh HPC scripts from git

--- repair_run.sh
+++
@@ -1,6 +0,0 @@
-#PBS -N repairdataset
-#PBS -m ae
-#PBS -M fmohammadi@ce.sharif.edu
-#PBS -l nodes=1:ppn=1
-cd GKmerhood
-/share/apps/Anaconda/anaconda3.7/bin/python3.7 repair.py > output_repair.txt
--- run_single_8_dataset.sh
+++
@@ -1,7 +0,0 @@
-#PBS -N single_8_dataset
-#PBS -m ae
-#PBS -M fmohammadi@ce.sharif.edu
-#PBS -l nodes=1:ppn=1
-cd GKmerhood
-/share/apps/Anaconda/anaconda3.7/bin/python3.7 GKmerhood.py > single_8_dataset.output
-
