caf5b30d0a012fef87d63bfd673c88ed3f1861db
Farzin Mohammadi <farzinlize@live.com>
2020-11-08 15:26:15 +0330

    replacing global variables as constants in a seperated module

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+# Global Variables
+DATASET_TREES = [('gkhood5_8', 'dataset')]
+HISTOGRAM_LOCATION = './results/figures/%s-f%d-d%d-q%d-g%d-o%d/'
+RESULT_LOCATION = './results/'
+BINDING_SITE_LOCATION = './data/answers.fasta'
+
+ARG_UNSET = -1
+FIND_MAX = -2
