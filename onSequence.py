8206da2fa9c036aad7639b7c08f09bae51ac8bf4
Farzin Mohammadi <farzinlize@live.com>
2021-04-03 17:57:43 +0430

    toByte implemented (byte2object still under implementation)

--- onSequence.py
+++ onSequence.py
@@ -1,6 +1,9 @@
+from ast import Bytes
+from io import BufferedReader
+from socket import socket
 from typing import List
 from TrieFind import WatchNode
-from misc import ExtraPosition
+from misc import ExtraPosition, int_to_bytes
 
 
 '''
@@ -21,6 +24,7 @@ class OnSequenceDistribution:
 
     def generate_list(self, motifs: List[WatchNode], sequences):
         self.struct = [[[] for _ in range(len(sequence))] for sequence in sequences]
+        self.margins = [[[] for _ in range(len(sequence))] for sequence in sequences]
         for motif in motifs:
             bundle = motif.foundmap.get_list()
             for index, seq_id in enumerate(bundle[0]):
@@ -28,16 +32,39 @@ class OnSequenceDistribution:
                 for position in bundle[1][index]:
                     try:
                         self.struct[seq_id][position.start_position] += [motif.label]
+                        self.margins[seq_id][position.start_position] += [position.size]
                     except IndexError:
                         print('[ERROR] IndexError raised | seq_id=%d, position=%d, len(sequence[index])=%d'%(
                             seq_id, position.start_position, len(sequences[index])
                         ))
 
+
+    def to_byte(self):
+        result = int_to_bytes(len(self.struct))
+        for sequence in self.struct:
+            result += int_to_bytes(len(sequence))
+            for positionlist in sequence:
+                result += len(positionlist)
+                for position in positionlist:
+                    result += int_to_bytes(len(position))
+                    result += bytes(position)
+        return result
+
+
+    @staticmethod
+    def byte_to_object_conn(bytes:Bytes):
+        pass
+        # conn.recv(2)
+
+
     def analysis(self):
         result = '[OnSequence][Analysis] id -> sequence id | z -> number of motifs in positions vector\n'
         total = 0
+        position_without_motif = 0
         for seq_id in range(len(self.struct)):
             z = sum([len(positionlist) for positionlist in self.struct[seq_id]])
+            if z == 0:
+                position_without_motif += 1
             total += z
             result += 'id:%d|z=%d\n'%(seq_id, z)
-        return result + '[OnSequence][Analysis] total=%d\n'%total
+        return result + '[OnSequence][Analysis] total=%d\t|\tempty positions -> %d\n'%(total, position_without_motif)
