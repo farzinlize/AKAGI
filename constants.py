47a13398addd9fea5bd68f7c89af8813f87fbfdc
Farzin Mohammadi <farzinlize@live.com>
2021-04-26 20:11:38 +0430

    minor change

--- constants.py
+++ constants.py
@@ -156,7 +156,7 @@ TABLE_HEADER_KEY = 3
 TOP_TEN_REPORT_HEADER = '****** [AKAGI] TOP TEN REPORT ******\n'
 
 MEME_FASTA_ID = True
-CLASSIC_MODE = '> id=%d\n%s\n'
+CLASSIC_MODE = '> id%d\n%s\n'
 MEME_MODE = '>%d\n%s\n'
 
 CHECKPOINT_TAG = '.checkpoint'
