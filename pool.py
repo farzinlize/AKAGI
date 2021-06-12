b265c4d6c6a9322eda0d2f88eabc29ae815904e9
Farzin Mohammadi <farzinlize@live.com>
2021-06-12 17:07:56 +0430

    saving pool on disk and recover

--- pool.py
+++ pool.py
@@ -1,6 +1,8 @@
+from FoundMap import FileMap
+import struct
 from typing import List
-from constants import CR_TABLE_HEADER_JASPAR, CR_TABLE_HEADER_SSMART, CR_TABLE_HEADER_SUMMIT, POOL_LIMITED, POOL_SIZE, PWM, P_VALUE, SEQUENCES, SEQUENCE_BUNDLES, SUMMIT, FUNCTION_KEY, ARGUMENT_KEY, SIGN_KEY, TABLE_HEADER_KEY, TOP_TEN_REPORT_HEADER
-from misc import ExtraPosition, binary_add_return_position, pwm_score_sequence
+from constants import APPDATA_PATH, CR_TABLE_HEADER_JASPAR, CR_TABLE_HEADER_SSMART, CR_TABLE_HEADER_SUMMIT, INT_SIZE, POOL_LIMITED, POOL_SIZE, PWM, P_VALUE, SEQUENCES, SEQUENCE_BUNDLES, SUMMIT, FUNCTION_KEY, ARGUMENT_KEY, SIGN_KEY, TABLE_HEADER_KEY, TOP_TEN_REPORT_HEADER
+from misc import ExtraPosition, binary_add_return_position, bytes_to_int, int_to_bytes, make_location, pwm_score_sequence
 from TrieFind import ChainNode
 
 class AKAGIPool:
@@ -24,7 +26,7 @@ class AKAGIPool:
 
     def __init__(self, pool_descriptions):
         self.descriptions = pool_descriptions
-        self.tables = []
+        self.tables: list[list[AKAGIPool.Entity]] = []
         for _ in pool_descriptions:self.tables.append([])
 
     
@@ -134,8 +136,38 @@ class AKAGIPool:
             self.tables[sorted_by] = merged
 
 
-    def savefile(self, filename):
-        pass
+    # ########################################## #
+    #            pool disk operations            #
+    # ########################################## #
+
+    def savefile(self, filename='akagi.pool'):
+        protected_directory = APPDATA_PATH + 'akagipool/'
+        make_location(protected_directory)
+        with open(filename, 'wb') as disk:
+            first = True # only first table instances must protect their data
+            for table in self.tables:
+                disk.write(int_to_bytes(len(table)))
+
+                for entity in table:
+                    scores_pack = struct.pack('d'*len(entity.scores), *entity.scores)
+                    disk.write(
+                        int_to_bytes(len(scores_pack)) +
+                        scores_pack +
+                        entity.data.to_byte(protect=first, directory=protected_directory)
+                    )
+                first = False
+
+
+    def readfile(self, filename='akagi.pool'):
+        with open(filename, 'rb') as disk:
+            for table_index in range(len(self.tables)):
+                table_len = bytes_to_int(disk.read(INT_SIZE))
+
+                for _ in range(table_len):
+                    pack_len = bytes_to_int(disk.read(INT_SIZE))
+                    scores = list(struct.unpack('d'*len(self.descriptions), disk.read(pack_len)))
+                    pattern = ChainNode.byte_to_object(disk)
+                    self.tables[table_index].append(AKAGIPool.Entity(pattern, scores))
 
 
 class RankingPool:
@@ -327,3 +359,42 @@ def get_AKAGI_pools_configuration(dataset_dict):
                 SIGN_KEY:-1, 
                 TABLE_HEADER_KEY:CR_TABLE_HEADER_JASPAR}
             ]
+
+
+if __name__ == '__main__':
+    pooly = AKAGIPool(get_AKAGI_pools_configuration({SEQUENCES:'', SEQUENCE_BUNDLES:'', PWM:''}))
+
+    a = FileMap()
+    a.add_location(0, ExtraPosition(6, 4))
+    a.add_location(0, ExtraPosition(6, 4))
+    a.add_location(1, ExtraPosition(6, 4))
+    a.add_location(2, ExtraPosition(6, 4))
+    a.add_location(2, ExtraPosition(6, 4))
+    a.add_location(3, ExtraPosition(6, 4))
+
+    b = FileMap()
+    b.add_location(0, ExtraPosition(8, 3))
+    b.add_location(1, ExtraPosition(8, 3))
+    b.add_location(1, ExtraPosition(8, 3))
+    b.add_location(2, ExtraPosition(8, 3))
+    b.add_location(2, ExtraPosition(8, 3))
+    b.add_location(5, ExtraPosition(8, 3))
+
+    pa = ChainNode('ATCGCCC', a)
+    pb = ChainNode('TTCGAG', b)
+
+    eb = AKAGIPool.Entity(pb, [12.67, 1.2, 6.7])
+    ea = AKAGIPool.Entity(pa, [2.4, 5.7, 3.9])
+
+    pooly.tables[0].append(ea)
+    pooly.tables[1].append(ea)
+    pooly.tables[2].append(ea)
+
+    pooly.tables[0].append(eb)
+    pooly.tables[1].append(eb)
+    pooly.tables[2].append(eb)
+
+    pooly.savefile()
+
+    pooly_disk = AKAGIPool(get_AKAGI_pools_configuration({SEQUENCES:'', SEQUENCE_BUNDLES:'', PWM:''}))
+    pooly_disk.readfile()
