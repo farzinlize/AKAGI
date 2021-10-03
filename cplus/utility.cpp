ee5e065ea531fd01890cc87d3fe37acc4341d0fd
farzin <farzinlize@live.com>
2021-10-03 07:36:43 -0700

    helper function for reading + pwm_find function

--- cplus/utility.cpp
+++ cplus/utility.cpp
@@ -10,6 +10,21 @@ int read_integer(FILE * data){
     return result;
 }
 
+double read_double(FILE * data){
+    double read;
+    fread(&read, sizeof(double), 1, data);
+    return read;
+}
+
+
+char * read_string(FILE * data, int size){
+    char * result = (char *) calloc(size+1, sizeof(char));
+    fread(result, sizeof(char), size, data);
+    return result;
+}
+
+
+/* just like read_string but using fgetc instead of fread */
 char * read_str(FILE * data, int size){
     char * result = (char *) malloc(size+1);
     for(int i=0;i<size;i++){
@@ -21,7 +36,7 @@ char * read_str(FILE * data, int size){
 
 void logit(char * message, char * logfile){
     FILE * log = fopen(logfile, "a");
-    fprintf(log, "%s", message);
+    fprintf(log, "%s\n", message);
     fclose(log);
 }
 
@@ -34,4 +49,18 @@ void put_integer(unsigned char * here, int n){
 
 int get_integer(unsigned char * here){
     return (here[3]) + (here[2] << 8) + (here[1] << 16) + (here[0] << 24);
+}
+
+double pwm_find(double * compact_pwm, int motif_size, char alphabet, int position, bool reverse){
+
+    int a;
+    switch (alphabet){
+    case 'A':a = 0;break;
+    case 'C':a = 1;break;
+    case 'G':a = 2;break;
+    case 'T':a = 3;break;
+    }
+
+    int position_on_array = a*motif_size*2 + (position*2 + reverse);
+    return compact_pwm[position_on_array];
 }
--- cplus/utility.h
+++ cplus/utility.h
@@ -8,9 +8,12 @@
 #define INTEGER_BYTES 4
 
 int read_integer(FILE * data);
+double read_double(FILE * data);
+char * read_string(FILE * data, int size);
 char * read_str(FILE * data, int size);
 void logit(char * message, char * logfile);
 void put_integer(unsigned char * here, int n);
 int get_integer(unsigned char * here);
+double pwm_find(double * compact_pwm, int motif_size, char alphabet, int position, bool reverse);
 
 #endif
