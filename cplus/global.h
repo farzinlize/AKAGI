35eb48ecadebdb038e14cc5e4d0984ae02387bbd
farzin <farzinlize@live.com>
2021-10-14 21:39:31 +0330

    global header for global constants

---
+++ cplus/global.h
@@ -0,0 +1,12 @@
+#ifndef _GLOBAL_DEFINES_H
+#define _GLOBAL_DEFINES_H
+
+/* compilation modes */
+#define DEBUG
+/*    which main     */
+#define MONGO_MAIN
+/* ---------------- */
+
+#define DATABASE_LOG "database.log"
+
+#endif
