4a2bbcbbf982f8e2537e06aaddeddaa88e35744d
farzin <farzinlize@live.com>
2021-10-25 15:55:29 +0330

    worker main program

--- cplus/worker.cpp
+++ cplus/worker.cpp
@@ -1,58 +1,153 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<unistd.h>
+#include "worker.h"
 
-#include"structures.h"
+/* global variables used by interrupt handler */
+bool MOTHER_ACTIVE = true;
+int  exit_code = EXIT_SUCCESS;
+mongoc_client_t * bank;
+int bank_port;
 
-//  global defines (constants)
+void mother_interrupt(int signum){
+    int signal = getchar();
+    switch(signal){
+    case SIGNAL_TERMINATE:
+    case EOF:
+        #ifndef OPTIMIZED
+        putchar(SIGNAL_BYE);
+        #endif
+        MOTHER_ACTIVE = false;break;
+    case SIGNAL_RESUME:
+        #ifndef OPTIMIZED
+        putchar(SIGNAL_ACK);
+        #endif
+        sleep(5);break;
+    case SIGNAL_CHANGE_BANK:
+        bank_port = read_integer(stdin);
+        #ifndef OPTIMIZED
+        putchar(SIGNAL_ACK);
+        #endif
+        mongoc_client_destroy(bank);
+        bank = get_client_c(bank_port);break;
+    default:
+        #ifndef OPTIMIZED
+        putchar(SIGNAL_NACK);
+        #endif
+        MOTHER_ACTIVE = false;
+        exit_code = EXIT_ERROR;
+    }
+    /* sending unecessary acknowledges to mother */
+    #ifndef OPTIMIZED
+    fflush(stdout);
+    #endif
+}
 
-#define REPORT_FILE_NAME "process_%d.report"
-#define NAME_BUFFER_SIZE 22
-#define EXIT_ERROR -1
+int error_handler(FILE * stream, int error, chain_node * popy){
+    switch (error){
+    case EMPTY:
+        fprintf(stream, "database empty (port:%d)\n", bank_port);
+        break;
+    case ESTORE:
+        //TODO: save popy in a file for furthur restoration
 
-#define BANK_PORT_INDEX 1
-#define ONSEQUENCE_INDEX 2
+    case EPOP:
+        fprintf(stream, "database failure (port:%d)\n", bank_port);
+        break;
+    }
 
-#define IMPORTANT_LOG '00_important_00.log'
-
-int empty_handler(){
+    /* wait for mother to respond*/
+    pause();
     return 0;
 }
 
-int error_handler(int error){
-    return 0;
-}
-
+#ifdef WORKER_MAIN
 int main(int argc, char* argv[]){
+    
+    /* defining variables */
+    clock_t now, record;
+    char message_buffer[REPORT_MESSAGE_BUFSIZE];
+    bool check, empty;
+    uint jobs_done_by_me = 0, chaining_done_by_me = 0, message_index;
 
     /* initiating report file with process id */
-    char report_file[NAME_BUFFER_SIZE];
+    char report_file[NAME_BUFFER_SIZE], errors_file[NAME_BUFFER_SIZE];
     sprintf(report_file, REPORT_FILE_NAME, getpid());
+    sprintf(errors_file, ERRORS_FILE_NAME, getpid());
     FILE * report = fopen(report_file, "w");
+    FILE * errors = fopen(errors_file, "w");
 
-    // parse arguments
-    if(argc != 3){fprintf(report, "EXIT ON ERROR - not a correct number of arguments");return EXIT_ERROR;}
+    /* declare signal handler to hear mother messages */
+    signal(SIGINT, mother_interrupt);
 
-    int bank_port = atoi(argv[BANK_PORT_INDEX]);
+    /* initial mongo driver */
+    mongoc_init();
+
+    /* parse arguments and load data */
+    if(argc != 7){fprintf(report, "EXIT ON ERROR - not a correct number of arguments\n");return EXIT_ERROR;}
+    bank_port = atoi(argv[BANK_PORT_INDEX]);
+    int judge_port = atoi(argv[JUDGE_PORT_INDEX]);
     on_sequence onsequence = open_on_sequence(argv[ONSEQUENCE_INDEX]);
+    dataset compact_data = load_compact_dataset(argv[COMPACT_INDEX]);
+    int overlap = atoi(argv[OVERLAP_INDEX]);
+    int gap = atoi(argv[GAP_INDEX]);
+    int q = atoi(argv[Q_INDEX]);
 
-    #ifdef TEST
-    printf("sequences count -> len(struct) -> %u\n", onsequence.sequence_count);
-    for(int i=0;i<200;i++)
-        printf("seq_id:%d-len(%d)", i, onsequence.sequence_lengths[i]);
-    // random check
-    int seq_id = 6, location = 27; 
-    char ** list = onsequence.data[seq_id][location];
-    char * current_str = list[0];
-    int count = 0;
-    while (current_str != NULL){
-        printf("(%s)", current_str);
-        current_str = list[++count];
+    /* connecting to other processes */
+    bank = get_client_c(bank_port);
+    int judge = connect_communication(judge_port);
+
+    while(MOTHER_ACTIVE){
+
+        /* starting message line for report */
+        message_index = 0;
+
+        /* obtaining a job */
+        chain_node motif;
+        check = pop_chain_node(bank, &motif, &empty);
+        if(!check){error_handler(errors, empty?EMPTY:EPOP, NULL);continue;}
+        jobs_done_by_me++;
+
+        /* evaluating the motif (drop on judge decide) */
+        double scores[3];
+        now = clock();       // [CAPTURE]
+        scores[SUMMIT_INDEX] = summit_score(compact_data, motif);
+        scores[SSMART_INDEX] = ssmart_score(compact_data, motif);
+        scores[JASPAR_INDEX] = pwm_score   (compact_data, motif);
+        record = clock() - now;
+        message_index += sprintf(&message_buffer[message_index], "[EVA:%ld]", record);
+        
+        /* talk with judge and ignore low ranks patterns */
+        now = clock();       // [CAPTURE]
+        check = send_report(judge, &motif, scores);
+        record = clock() - now;
+        if(!check) continue;
+        message_index += sprintf(&message_buffer[message_index], "[JUG:%ld]", record);
+        
+        /* chaining */
+        now = clock();       // [CAPTURE]
+        chain_link next_generation = 
+            next_chain(motif, onsequence, compact_data.sequence_count, overlap, gap, q);
+        record = clock() - now;
+        message_index += sprintf(&message_buffer[message_index], "[CHN:%ld]", record);
+        
+        /* storing next generation */
+        now = clock();       // [CAPTURE]
+        check = store_many_chains(next_generation, bank);
+        record = clock() - now;
+        if(!check){error_handler(errors, ESTORE, &motif);continue;}
+        message_index += sprintf(&message_buffer[message_index], "[DBS:%ld]", record);
+
+        /* clean trash */
+        destroy_node(&motif);
+
+        /* report to user in file */
+        chaining_done_by_me++;
+        message_buffer[message_index++] = '\x00';
+        fprintf(report, "%s\n", message_buffer);fflush(report);
     }
-    printf("\ncount = %d", count);
-    #endif
+    
+    /* clean ending */
+    mongoc_client_destroy(bank);
+    mongoc_cleanup();
 
-    printf("HOW MUCH IS DONE HAS DONE ANYWAY");
     return 0;
 }
+#endif
