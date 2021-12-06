fc81fa18a9267093a80ddf0c7d5f52fb88d436b5
farzin <farzinlize@live.com>
2021-12-06 17:25:16 +0330

    resolve algorithms problem (next_chain and pwm_score)

--- cplus/algorithm.cpp
+++ cplus/algorithm.cpp
@@ -2,28 +2,54 @@
 
 chain_link * next_chain(chain_node motif, on_sequence on_seq, int sequences_count, int overlap, int gap, int q){
     
-    /*   observation   */
+    #ifdef DEBUG_CHAINING
+    int number_of_observation = 0;
+    #endif
+
+    /* observation for chaining */
     tree_node * observation = initial_tree(NULL);
     FoundMap * current_map = motif.foundmap;
-    while(current_map != NULL){                                 //sequences
+
+    /* sequences (scroll) */
+    while(current_map != NULL){
         int current_seq_id = current_map->seq_id;
         pos_link * current_position = current_map->positions;
-        while (current_position != NULL){                       //positions
-            for (int sliding=-overlap;sliding<=gap;sliding++){  // sliding
-                int next_position_location = current_position->location + current_position->size + sliding;
+
+        /* positions (scroll) */
+        while (current_position != NULL){
+
+            /* sliding (overlap-gap) */
+            for (int sliding=-overlap;sliding<=gap;sliding++){
+                int end_position = current_position->location + current_position->size;
+                int next_position_location = end_position + sliding;
+
+                /* ignore sliding value that passes through the sequence or the whole motif itself */
                 if(next_position_location >= on_seq.sequence_lengths[current_seq_id] || next_position_location <= current_position->location)continue;
-                int next_position_size = current_position->size;
-                int i = 0; char * zero_motif = on_seq.data[current_seq_id][next_position_location][i++];
-                while (zero_motif != NULL){                  // zero-motifs
-                    add_frame(observation, zero_motif, current_seq_id, next_position_location, next_position_size, 0);
-                    zero_motif = on_seq.data[current_seq_id][next_position_location][i++];
+                
+                /* zero-motifs as condidate */
+                int i = 0; char * zero_motif;
+                while ((zero_motif = on_seq.data[current_seq_id][next_position_location][i++]) != NULL){
+                    int candidate_size = strlen(zero_motif);
+
+                    /* ignore candidates which doesn't extend the motif length */
+                    /* TODO IMPORTANT WARNING CHECK HERE THIS LINE after or condition */
+                    if(next_position_location + candidate_size <= end_position || next_position_location + candidate_size >= on_seq.sequence_lengths[current_seq_id])continue;
+
+                    add_frame(observation, zero_motif, current_seq_id, current_position->location, current_position->size + candidate_size + sliding, 0);
+
+                    #ifdef DEBUG_CHAINING
+                    number_of_observation++;
+                    #endif
                 }
             }current_position = current_position->next;
         }current_map = current_map->next;
     }
 
-    /*   next-generation extreaction   */   
-    /* initial result array         */
+    #ifdef DEBUG_CHAINING
+    printf("[CHAINING] number of observation -> %d\n", number_of_observation);
+    #endif
+
+    /* next-generation extreaction */   
     chain_link * head = initial_empty_chain_link();
     chain_link * tail = head;
 
@@ -48,7 +74,7 @@ chain_link * next_chain(chain_node motif, on_sequence on_seq, int sequences_coun
         if(current_node.q >= q){
             chain_node * new_chain = (chain_node *) malloc(sizeof(chain_node));
             new_chain->foundmap = current_node.foundmap;
-            new_chain->label = current_node.label;
+            new_chain->label = concat_malloc(motif.label, current_node.label);
             new_chain->foundmap_mode = FOUNDMAP_NOARRAY;
             preserve = true;
 
@@ -85,8 +111,17 @@ double pwm_score(dataset data, chain_node pattern){
             start = instance->location;
             end   = instance->location + instance->size;
 
+            /* prevent going through sequence length for extended motifs */
+            /* motifs may extend sequences in an edit-distance based alignment */
+            if(end > s_len[seq_id]) end = s_len[seq_id];
+
             /* calculating score of instance */
             double max_score = -DBL_MAX; int range = instance->size + pwm_size;
+
+            #ifdef DEBUG_PWM
+            printf("[INSTANCE] size-> %d, location-> %d (seq_id=%d)\n", instance->size, instance->location, seq_id);
+            #endif
+
             for(int i=1; i<range; i++){
                 int seq_idx = max_integer(start, start + i - pwm_size);
                 int ref_idx = max_integer(0, pwm_size - i);
@@ -96,14 +131,39 @@ double pwm_score(dataset data, chain_node pattern){
                     a_score = a_score + pwm_find(pwm, pwm_size, sequences[seq_id][seq_idx], ref_idx, false);
                     r_score = r_score + pwm_find(pwm, pwm_size, sequences[seq_id][seq_idx], ref_idx, true);
                     ref_idx++; seq_idx++;
+
+                    #ifdef DEBUG_PWM
+                    printf("(seq_idx=%d, character=%c, ref_idx=%d)\t", seq_idx, sequences[seq_id][seq_idx], ref_idx);
+                    #endif
                 }
 
+                #ifdef DEBUG_PWM
+                if(a_score > max_score) {
+                    max_score = a_score;
+                    printf("#(a:%f)#", a_score);
+                }
+                if(r_score > max_score) {
+                    max_score = r_score;
+                    printf("#(r:%f)#", r_score);
+                }
+                printf("#(max-score-until-now->%f)#\n", max_score);
+                #else
                 if(a_score > max_score) max_score = a_score;
                 if(r_score > max_score) max_score = r_score;
+                #endif
             }
+
+            #ifdef DEBUG_PWM
+            printf("[DEBUG][PWM] instance max_score -> %f\n", max_score);
+            #endif
+
             a = a + max_score; count++;
         }
     }
+
+    #ifdef DEBUG_PWM
+    printf("[DEBUG][PWM] a=%f, count=%d\n", a, count);
+    #endif
 
     /* returing the average as pattern score */
     return a / count;
@@ -171,10 +231,11 @@ int main(int argc, char * argv[]){
     uint8_t * bin = (uint8_t*) calloc(bin_size, sizeof(uint8_t));
     fread(bin, sizeof(uint8_t), bin_size, motif_data);
     motif.foundmap = binary_to_structure(bin);
-    dataset compact = load_compact_dataset("test.dataset");
+    dataset compact = load_compact_dataset("test.ct");
     on_sequence onseq = open_on_sequence("test.onseq");
-    printf("[ALG] all data loaded - investigating chain node (label:%s)\n", motif.label);
+    printf("[ALG] all data loaded - investigating chain node (label:%s, bin_size:%d)\n", motif.label, bin_size);
 
+    #ifdef TEST_ONSEQ
     char * test_motif = onseq.data[0][0][0];
     printf("first motif in onseq (0.0.0) -> %s, size=%ld\n", test_motif, strlen(test_motif));
     int a = 1;
@@ -182,6 +243,7 @@ int main(int argc, char * argv[]){
         test_motif = onseq.data[0][0][a++];
     }
     printf("number of motifs at (0.0) -> %d\n", a);
+    #endif
 
     /* scores */
     double scores[3];
@@ -194,11 +256,12 @@ int main(int argc, char * argv[]){
 
     /* chaining */
     now = clock();
-    chain_link * result = next_chain(motif, onseq, onseq.sequence_count, 4, 4, 200);
+    chain_link * result = next_chain(motif, onseq, onseq.sequence_count, 4, 4, 195);
     record = clock() - now;
     printf("[CHAIN] number of clocks -> %ld (clock per second = %ld)\n", record, CLOCKS_PER_SEC);
     chain_link *current = result;
-    int next_size = 0;
+    int next_size = 0, nexty_idx = 0;
+    char filename[200];
     while(current != NULL){
         if(current->node == NULL) {
             printf("[?] NULL node in chain_link\n");
@@ -206,10 +269,16 @@ int main(int argc, char * argv[]){
         }
         next_size++;
         chain_node * nexty = current->node;
+        
         uint32_t bin_size;
-        structure_to_binary(nexty->foundmap, &bin_size);
-        printf("%d, %s\n", bin_size, nexty->label);
+        uint8_t * data_bin = structure_to_binary(nexty->foundmap, &bin_size);
+        sprintf(filename, "nexty[%d].data", nexty_idx);
+        FILE * f = fopen(filename, "wb");
+        fwrite(data_bin, sizeof(uint8_t), bin_size, f);
+
+        printf("bin_size->%d, label->%s\n", bin_size, nexty->label);
         current = current->next;
+        nexty_idx++;
     }printf("[CHAIN] number of next chains = %d\n", next_size);
 
     return 0;
