0a59f0bb0a2d78255428afc716715bb8c56b33ac
Farzin Mohammadi <farzinlize@live.com>
2022-02-02 22:28:59 +0330

    restore dumped file (also pwm_score from sequences -> doc version)

--- doctor.py
+++ doctor.py
@@ -1,11 +1,13 @@
+from io import BytesIO
 import sys, os
+from FoundMap import MemoryMap
 from GKmerhood import GKHoodTree
-from TrieFind import initial_chainNodes
+from TrieFind import ChainNode, initial_chainNodes
 from checkpoint import load_checkpoint_file, load_collection
-from constants import BANK_NAME, BANK_PATH, CHECKPOINT_TAG, DATASET_TREES, DNA_ALPHABET, MONGOD_SHUTDOWN_COMMAND, QUEUE_COLLECTION
+from constants import BANK_NAME, BANK_PATH, CHECKPOINT_TAG, DATASET_TREES, DNA_ALPHABET, INT_SIZE, MONGOD_SHUTDOWN_COMMAND, QUEUE_COLLECTION
 from mongo import get_bank_client, initial_akagi_database
 from onSequence import OnSequenceDistribution
-from misc import read_fasta, read_bundle, brief_sequence
+from misc import binary_to_list, bytes_to_int, pwm_score_sequence, read_fasta, read_bundle, brief_sequence
 
 def test_gkhood_dataset_k(gkhood_index, k):
     tree = GKHoodTree(DATASET_TREES[gkhood_index][0], DATASET_TREES[gkhood_index][1])
@@ -74,6 +76,37 @@ def manual_initial(bank_order, initial_works_address:str, dataset, onsequence_na
 
     # shutdown banks after work
     for index in range(len(manual_ports)):os.system(MONGOD_SHUTDOWN_COMMAND%(BANK_PATH%index))
+
+
+def pwm_score_doc(sequences, pa, pr):
+    aggregated = 0
+    for seq in sequences:
+        a, _ = pwm_score_sequence(seq, pa)
+        r, _ = pwm_score_sequence(seq, pr)
+        aggregated+=max(a, r)
+    return aggregated / len(sequences)
+
+
+def restore_dumped_motifs(dumped='dumped.motifs', back_bank=None):
+    with open(dumped, 'rb') as dumpy:
+        restored = []
+        corrupted = False
+        while True:
+            read = dumpy.read(INT_SIZE)
+            if not read:break
+            label_len = bytes_to_int(read, endian='little')
+            label = str(dumpy.read(label_len), 'utf8')
+            if not label:corrupted = True;break
+            read = dumpy.read(INT_SIZE)
+            if not read:corrupted = True;break
+            data_size = bytes_to_int(read, endian='little')
+            bin_data = dumpy.read(data_size)
+            if not bin_data:corrupted = True;break
+            found_list = binary_to_list(BytesIO(bin_data))
+            restored.append(ChainNode(label, MemoryMap(initial=found_list)))
+    
+    if not back_bank:return restored, corrupted
+    return initial_chainNodes([(m.label, m.foundmap) for m in restored], QUEUE_COLLECTION, get_bank_client(back_bank)), corrupted
 
 
 if __name__ == "__main__":
