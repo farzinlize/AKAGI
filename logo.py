c66f6ac454cfda379ebbbdb1761b7b01d3cac53f
Farzin Mohammadi <farzinlize@live.com>
2022-06-15 17:52:23 +0430

    minor changes in logo analysis

--- logo.py
+++ logo.py
@@ -16,7 +16,7 @@ def make_pattern_report(name, pattern:ChainNode, sequences, directory=''):
                 if data_frame[alphabet][i] < 0:data_frame[alphabet][i] = 0
 
     make_location(directory)
-    cal = lambda col, row:log2( ( (col[row]+(PSEUDOCOUNT/4) ) / (A[row]+C[row]+G[row]+T[row]+PSEUDOCOUNT) )/0.25 )
+    cal = lambda col, row, count:log2( ( (col[row]+(PSEUDOCOUNT/4) ) / (count+PSEUDOCOUNT) )/0.25 )
     
     binding_sites = pattern.instances_list(sequences)
     aligned_sites = alignment_strings(binding_sites)
@@ -27,7 +27,7 @@ def make_pattern_report(name, pattern:ChainNode, sequences, directory=''):
     shifted_counts = [c-average_count for c in position_counts]
     log_shifted_counts = [log(c/average_count) for c in position_counts]
     
-    _, start, end = maxSubArraySum(shifted_counts)
+    _, start, end         = maxSubArraySum(shifted_counts)
     _, log_start, log_end = maxSubArraySum(log_shifted_counts)
 
     plt.plot(position_counts)
@@ -60,40 +60,50 @@ def make_pattern_report(name, pattern:ChainNode, sequences, directory=''):
     plt.savefig(f'{directory}{name}_bs_alignment.png')
     plt.clf()
 
-    A = list(aligned_matrix['A'][start:end+1]);Al = list(aligned_matrix['A'][log_start:log_end+1])
-    C = list(aligned_matrix['C'][start:end+1]);Cl = list(aligned_matrix['C'][log_start:log_end+1])
-    T = list(aligned_matrix['T'][start:end+1]);Tl = list(aligned_matrix['T'][log_start:log_end+1])
-    G = list(aligned_matrix['G'][start:end+1]);Gl = list(aligned_matrix['G'][log_start:log_end+1])
+    A = list(aligned_matrix['A'][start:end+1])
+    C = list(aligned_matrix['C'][start:end+1])
+    T = list(aligned_matrix['T'][start:end+1])
+    G = list(aligned_matrix['G'][start:end+1])
 
-    r_start = len(aligned_matrix) - end
-    r_end   = len(aligned_matrix) - start
-    r_log_start = len(aligned_matrix) - log_end
-    r_log_end   = len(aligned_matrix) - log_start
+    Al = list(aligned_matrix['A'][log_start:log_end+1])
+    Cl = list(aligned_matrix['C'][log_start:log_end+1])
+    Tl = list(aligned_matrix['T'][log_start:log_end+1])
+    Gl = list(aligned_matrix['G'][log_start:log_end+1])
 
-    rA = list(reversed(list(aligned_matrix['T'][r_start:r_end+1])));rAl = list(reversed(list(aligned_matrix['T'][r_log_start:r_log_end+1])))
-    rC = list(reversed(list(aligned_matrix['G'][r_start:r_end+1])));rCl = list(reversed(list(aligned_matrix['G'][r_log_start:r_log_end+1])))
-    rG = list(reversed(list(aligned_matrix['C'][r_start:r_end+1])));rGl = list(reversed(list(aligned_matrix['C'][r_log_start:r_log_end+1])))
-    rT = list(reversed(list(aligned_matrix['A'][r_start:r_end+1])));rTl = list(reversed(list(aligned_matrix['A'][r_log_start:r_log_end+1])))
+    # r_start     = len(aligned_matrix) - end
+    # r_end       = len(aligned_matrix) - start
+    # r_log_start = len(aligned_matrix) - log_end
+    # r_log_end   = len(aligned_matrix) - log_start
 
-    data = {'A':[cal(A, i) for i in range(len(A))], 
-            'T':[cal(T, i) for i in range(len(T))], 
-            'C':[cal(C, i) for i in range(len(C))], 
-            'G':[cal(G, i) for i in range(len(G))]}
+    rA = list(reversed(list(aligned_matrix['T'][start:end+1])))
+    rC = list(reversed(list(aligned_matrix['G'][start:end+1])))
+    rG = list(reversed(list(aligned_matrix['C'][start:end+1])))
+    rT = list(reversed(list(aligned_matrix['A'][start:end+1])))
+
+    rAl = list(reversed(list(aligned_matrix['T'][log_start:log_end+1])))
+    rCl = list(reversed(list(aligned_matrix['G'][log_start:log_end+1])))
+    rGl = list(reversed(list(aligned_matrix['C'][log_start:log_end+1])))
+    rTl = list(reversed(list(aligned_matrix['A'][log_start:log_end+1])))
+
+    data = {'A':[cal(A, i, A[i]+T[i]+C[i]+G[i]) for i in range(len(A))], 
+            'T':[cal(T, i, A[i]+T[i]+C[i]+G[i]) for i in range(len(T))], 
+            'C':[cal(C, i, A[i]+T[i]+C[i]+G[i]) for i in range(len(C))], 
+            'G':[cal(G, i, A[i]+T[i]+C[i]+G[i]) for i in range(len(G))]}
     
-    rdata = {'A':[cal(rA, i) for i in range(len(rA))], 
-             'T':[cal(rT, i) for i in range(len(rT))], 
-             'C':[cal(rC, i) for i in range(len(rC))], 
-             'G':[cal(rG, i) for i in range(len(rG))]}
+    rdata = {'A':[cal(rA, i, rA[i]+rT[i]+rC[i]+rG[i]) for i in range(len(rA))], 
+             'T':[cal(rT, i, rA[i]+rT[i]+rC[i]+rG[i]) for i in range(len(rT))], 
+             'C':[cal(rC, i, rA[i]+rT[i]+rC[i]+rG[i]) for i in range(len(rC))], 
+             'G':[cal(rG, i, rA[i]+rT[i]+rC[i]+rG[i]) for i in range(len(rG))]}
 
-    datal = {'A':[cal(Al, i) for i in range(len(Al))], 
-             'T':[cal(Tl, i) for i in range(len(Tl))], 
-             'C':[cal(Cl, i) for i in range(len(Cl))], 
-             'G':[cal(Gl, i) for i in range(len(Gl))]}
+    datal = {'A':[cal(Al, i, Al[i]+Tl[i]+Cl[i]+Gl[i]) for i in range(len(Al))], 
+             'T':[cal(Tl, i, Al[i]+Tl[i]+Cl[i]+Gl[i]) for i in range(len(Tl))], 
+             'C':[cal(Cl, i, Al[i]+Tl[i]+Cl[i]+Gl[i]) for i in range(len(Cl))], 
+             'G':[cal(Gl, i, Al[i]+Tl[i]+Cl[i]+Gl[i]) for i in range(len(Gl))]}
     
-    rdatal = {'A':[cal(rAl, i) for i in range(len(rAl))], 
-              'T':[cal(rTl, i) for i in range(len(rTl))], 
-              'C':[cal(rCl, i) for i in range(len(rCl))], 
-              'G':[cal(rGl, i) for i in range(len(rGl))]}
+    rdatal = {'A':[cal(rAl, i, rAl[i]+rTl[i]+rCl[i]+rGl[i]) for i in range(len(rAl))], 
+              'T':[cal(rTl, i, rAl[i]+rTl[i]+rCl[i]+rGl[i]) for i in range(len(rTl))], 
+              'C':[cal(rCl, i, rAl[i]+rTl[i]+rCl[i]+rGl[i]) for i in range(len(rCl))], 
+              'G':[cal(rGl, i, rAl[i]+rTl[i]+rCl[i]+rGl[i]) for i in range(len(rGl))]}
 
     df   = DataFrame(data)  ;no_negative(df)
     rdf  = DataFrame(rdata) ;no_negative(rdf)
